9e1d02421884a41dccb46369d04b8359a12cdd1c
amr <amr@49feb11a-dc6b-4521-947f-5d0cd0a85b6f>
2016-09-02 15:55:03 +0000

    -work on implementing command line argument parsing
    git-svn-id: https://svn.broadinstitute.org/BTL/Parsomatic@1096 49feb11a-dc6b-4521-947f-5d0cd0a85b6f

--- src/main/scala/org/broadinstitute/parsomatic/Parsomatic.scala
+++ src/main/scala/org/broadinstitute/parsomatic/Parsomatic.scala
@@ -5,22 +5,34 @@ import java.io.File
   * Created by amr on 9/1/2016.
   */
 
-object Parsomatic extends App{
+object Parsomatic extends App {
   def parser = {
     new scopt.OptionParser[Config]("Parsomatic") {
-    head("Parsomatic", "1.0")
-    opt[File]('f', "inputFile").required().valueName("<file>").action((x,c) => c.copy(inputFile = x)).
-      text("Input file to parse.")
-    opt[Int]('h', "headerRow").action((x,c) => c.copy(headerRow = x)).text("Header row in file. Default = 1.")
-    opt[Int]('l', "lastRow").action((x,c) => c.copy(lastRow = x)).text("Last row of data to parse.")
-    opt[String]('s', "startKey").action((x,c) => c.copy(startKey = x)).text("A key string to indicate header row.")
-    opt[String]('e', "endKey").action((x,c) => c.copy(endKey = x)).text("A string to indicate last row of data.")
-    opt[String]('d', "delimiter").required().action((x, c) => c.copy(delimiter = x)).
-      text("Delimiter used to separate values in file.")
-    help("help").text("Prints this help text.")
+      head("Parsomatic", "1.0")
+      opt[File]('i', "inputFile").valueName("<file>").required().action((x, c) => c.copy(inputFile = x))
+        .text("Path to input file to parse. Required.")
+      opt[Int]('h', "headerRow").valueName("<int>").action((x, c) => c.copy(headerRow = x))
+        .text("Header row in file. Default = 1.")
+      opt[Int]('l', "lastRow").valueName("<int>").optional().action((x, c) => c.copy(lastRow = x))
+        .text("Last row of data to parse. If unspecified, will end parse all lines after headerRow.")
+      opt[String]('s', "startKey").valueName("<string>").optional().action((x, c) => c.copy(startKey = x))
+        .text("A key string to indicate header row. Must begin with first character of line.")
+      opt[String]('e', "endKey").valueName("<string>").optional().action((x, c) => c.copy(endKey = x))
+        .text("A string to indicate last row of data. Must begin with first character of line.")
+      opt[String]('d', "delimiter").valueName("<char>").required().action((x, c) => c.copy(delimiter = x))
+        .text("Delimiter used to separate values in file. Use '\\t' for tabs. Required.")
+      help("help").text("Prints this help text.")
+      note("\nA tool for parsing data files into MD objects.\n")
     }
   }
-//  parser.parse(args, Config(inputFile = )) match {
-//    case Some(config) => //do stuff
-//  }
+  private def failureExit(msg: String) {
+    println("\nFATAL ERROR: " + msg)
+    System.exit(1)
+  }
+
+  parser.parse(args, Config(inputFile = new File("NA.tmp"), lastRow = 0, headerRow = 1, startKey = "foo", endKey = "bar", delimiter = "\t")) match {
+    // Command line arguments are valid - go execute them
+    case Some(config) => println(config.inputFile.toString)
+    case None => failureExit("Please provide valid input.")
+  }
 }
