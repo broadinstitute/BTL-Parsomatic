2aaadf4275904ac5a6d23a4d37793f8578fb3ff1
amr <amr@49feb11a-dc6b-4521-947f-5d0cd0a85b6f>
2016-09-02 15:55:56 +0000

    -work on implementing command line argument parsing
    git-svn-id: https://svn.broadinstitute.org/BTL/Parsomatic@1097 49feb11a-dc6b-4521-947f-5d0cd0a85b6f

--- src/main/scala/org/broadinstitute/parsomatic/Parsomatic.scala
+++ src/main/scala/org/broadinstitute/parsomatic/Parsomatic.scala
@@ -32,7 +32,7 @@ object Parsomatic extends App {
 
   parser.parse(args, Config(inputFile = new File("NA.tmp"), lastRow = 0, headerRow = 1, startKey = "foo", endKey = "bar", delimiter = "\t")) match {
     // Command line arguments are valid - go execute them
-    case Some(config) => println(config.inputFile.toString)
+    case Some(config) => println("I'm doing something")
     case None => failureExit("Please provide valid input.")
   }
 }
