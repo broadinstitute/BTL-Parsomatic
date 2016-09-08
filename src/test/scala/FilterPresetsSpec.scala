fa3c901f1a72db328c9f0e0f5411de57b016ab07
amr <amr@49feb11a-dc6b-4521-947f-5d0cd0a85b6f>
2016-09-08 17:23:34 +0000

    -added example input files to resources -worked on FilterPresetsSpec
    git-svn-id: https://svn.broadinstitute.org/BTL/Parsomatic@1104 49feb11a-dc6b-4521-947f-5d0cd0a85b6f

--- src/test/scala/FilterPresetsSpec.scala
+++ src/test/scala/FilterPresetsSpec.scala
@@ -1,13 +1,25 @@
 import org.broadinstitute.parsomatic.FilterPresets._
-import org.scalatest.WordSpec
+import org.broadinstitute.parsomatic.Parsomatic.filterResultHandler
+import org.scalatest.{FlatSpec, Matchers, WordSpec}
 
 /**
   * Created by Amr on 9/6/2016.
   */
-class FilterPresetsSpec extends WordSpec {
-  "filteredByRow" when {
-    val picardPreset = new PicardMetricPreset("C:\\Dev\\Scala\\Parsomatic\\src\\test\\resources\\Mouse-A2-single.AlignmentSummaryMetrics.out")
-    "doing something" should {
-    }
+
+class FilterPresetsSpec extends FlatSpec with Matchers {
+  val alignmentMetrics = "C:\\Dev\\Scala\\Parsomatic\\src\\test\\resources\\Mouse-A2-single.AlignmentSummaryMetrics.out"
+  val insertMetrics = "C:\\Dev\\Scala\\Parsomatic\\src\\test\\resources\\Mouse-A2-single.InsertSize.out"
+  val gcMetrics = "C:\\Dev\\Scala\\Parsomatic\\src\\test\\resources\\Mouse-A2-single.ReadGCMetrics.out"
+
+  "A PicardMetricPreset" should "return a header row as first element of iterable from alignment summary file" in {
+    val filteredAlignment = new PicardMetricPreset(alignmentMetrics)
+    val run = filteredAlignment.run()
+    filteredAlignment.start should be (7)
+    filteredAlignment.run().isRight should be (true)
   }
+
+//  "A PicardHistMetricPreset" should "return an iterable representation of a file" in {
+//    val parsedInsert = new PicardMetricPreset(insertMetrics)
+//    val parsedGc = new PicardMetricPreset(gcMetrics)
+//  }
 }
