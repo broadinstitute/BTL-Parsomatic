e5b35e272a29eca16860f2d337018afc1dc7fabd
amr <amr@49feb11a-dc6b-4521-947f-5d0cd0a85b6f>
2016-10-05 15:09:54 +0000

    -last of the unit tests I will implement at this time.
    git-svn-id: https://svn.broadinstitute.org/BTL/Parsomatic@1176 49feb11a-dc6b-4521-947f-5d0cd0a85b6f

--- src/main/scala/org/broadinstitute/parsomatic/MapToAnalysisObject.scala
+++ src/main/scala/org/broadinstitute/parsomatic/MapToAnalysisObject.scala
@@ -48,56 +48,56 @@ class MapToAnalysisObject(mdType: String, input: List[Map[String, String]]) {
         for (row <- input) {
         metrics += new PicardAlignmentSummaryMetrics(
           category = row.getOrElse("CATEGORY", "N/A"),
-          totalReads = convertToInt(row, "TOTAL_READS", "0"),
-          pfReads = convertToInt(row, "PF_READS", "0"),
-          pctPfReads = convertToDouble(row, "PCT_PF_READS", "0.0"),
-          pfNoiseReads = convertToInt(row, "PF_NOISE_READS", "0"),
-          pfReadsAligned = convertToInt(row, "PF_READS_ALIGNED", "0"),
-          pctPfReadsAligned = convertToDouble(row, "PCT_PF_READS_ALIGNED", "0.0"),
-          pfAlignedBases = convertToInt(row, "PF_ALIGNED_BASES", "0"),
-          pfHqAlignedReads = convertToInt(row, "PF_ALIGNED_BASES", "0"),
-          pfHqAlignedBases = convertToInt(row, "PF_ALIGNED_BASES", "0"),
-          pfHqAlignedQ20Bases = convertToInt(row, "PF_HQ_ALIGNED_20_BASES", "0"),
-          pfHqMedianMismatches = convertToDouble(row, "PF_HQ_MEDIAN_MISMATCHES", "0.0"),
-          pfMismatchRate = convertToDouble(row, "PF_MISMATCH_RATE", "0.0"),
-          pfHqErrorRate = convertToDouble(row, "PF_HQ_ERROR_RATE", "0.0"),
-          pfIndelRate = convertToDouble(row, "PF_INDEL_RATE", "0.0"),
-          meanReadLength = convertToInt(row, "MEAN_READ_LENGTH", "0"),
-          readsAlignedInPairs = convertToInt(row, "READS_ALIGNED_IN_PAIRS", "0"),
-          pctReadsAlignedInPairs = convertToDouble(row, "PCT_READS_ALIGNED_IN_PAIRS", "0.0"),
-          badCycles = convertToInt(row, "BAD_CYCLES", "0"),
-          strandBalance = convertToInt(row, "STRAND_BALANCE", "0"),
-          pctChimeras = convertToDouble(row, "PCT_CHIMERAS", "0.0"),
-          pctAdapter = convertToDouble(row, "PCT_ADAPTER", "0.0")
+          totalReads = convertToInt(row, "TOTAL_READS", "-1"),
+          pfReads = convertToInt(row, "PF_READS", "-1"),
+          pctPfReads = convertToDouble(row, "PCT_PF_READS", "-1.0"),
+          pfNoiseReads = convertToInt(row, "PF_NOISE_READS", "-1"),
+          pfReadsAligned = convertToInt(row, "PF_READS_ALIGNED", "-1"),
+          pctPfReadsAligned = convertToDouble(row, "PCT_PF_READS_ALIGNED", "-1.0"),
+          pfAlignedBases = convertToInt(row, "PF_ALIGNED_BASES", "-1"),
+          pfHqAlignedReads = convertToInt(row, "PF_ALIGNED_BASES", "-1"),
+          pfHqAlignedBases = convertToInt(row, "PF_ALIGNED_BASES", "-1"),
+          pfHqAlignedQ20Bases = convertToInt(row, "PF_HQ_ALIGNED_20_BASES", "-1"),
+          pfHqMedianMismatches = convertToDouble(row, "PF_HQ_MEDIAN_MISMATCHES", "-1.0"),
+          pfMismatchRate = convertToDouble(row, "PF_MISMATCH_RATE", "-1.0"),
+          pfHqErrorRate = convertToDouble(row, "PF_HQ_ERROR_RATE", "-1.0"),
+          pfIndelRate = convertToDouble(row, "PF_INDEL_RATE", "-1.0"),
+          meanReadLength = convertToInt(row, "MEAN_READ_LENGTH", "-1"),
+          readsAlignedInPairs = convertToInt(row, "READS_ALIGNED_IN_PAIRS", "-1"),
+          pctReadsAlignedInPairs = convertToDouble(row, "PCT_READS_ALIGNED_IN_PAIRS", "-1.0"),
+          badCycles = convertToInt(row, "BAD_CYCLES", "-1"),
+          strandBalance = convertToInt(row, "STRAND_BALANCE", "-1"),
+          pctChimeras = convertToDouble(row, "PCT_CHIMERAS", "-1.0"),
+          pctAdapter = convertToDouble(row, "PCT_ADAPTER", "-1.0")
           )
         }
         Right(PicardAlignmentSummaryAnalysis(metrics.toList))
       case "PicardInsertSizeMetrics" =>
         Right(PicardInsertSizeMetrics(
-          medianInsertSize = convertToInt(input.head, "MEDIAN_INSERT_SIZE", "0"),
-          medianAbsoluteDeviation = convertToInt(input.head, "MEDIAN_ABSOLUTE_DEVIATION", "0"),
-          minInsertSize = convertToInt(input.head, "MIN_INSERT_SIZE", "0"),
-          maxInsertSize = convertToInt(input.head, "MAX_INSERT_SIZE", "0"),
-          meanInsertSize = convertToDouble(input.head, "MEAN_INSERT_SIZE", "0.0"),
-          standardDeviation = convertToDouble(input.head, "STANDARD_DEVIATION", "0.0"),
-          readPairs = convertToInt(input.head, "READ_PAIRS", "0"),
+          medianInsertSize = convertToInt(input.head, "MEDIAN_INSERT_SIZE", "-1"),
+          medianAbsoluteDeviation = convertToInt(input.head, "MEDIAN_ABSOLUTE_DEVIATION", "-1"),
+          minInsertSize = convertToInt(input.head, "MIN_INSERT_SIZE", "-1"),
+          maxInsertSize = convertToInt(input.head, "MAX_INSERT_SIZE", "-1"),
+          meanInsertSize = convertToDouble(input.head, "MEAN_INSERT_SIZE", "-1.0"),
+          standardDeviation = convertToDouble(input.head, "STANDARD_DEVIATION", "-1.0"),
+          readPairs = convertToInt(input.head, "READ_PAIRS", "-1"),
           pairOrientation = input.head.getOrElse("PAIR_ORIENTATION", "N/A"),
-          widthOf10Pct = convertToInt(input.head, "WIDTH_OF_10_PERCENT", "0"),
-          widthOf20Pct = convertToInt(input.head, "WIDTH_OF_20_PERCENT", "0"),
-          widthOf30Pct = convertToInt(input.head, "WIDTH_OF_30_PERCENT", "0"),
-          widthOf40Pct = convertToInt(input.head, "WIDTH_OF_40_PERCENT", "0"),
-          widthOf50Pct = convertToInt(input.head, "WIDTH_OF_50_PERCENT", "0"),
-          widthOf60Pct = convertToInt(input.head, "WIDTH_OF_60_PERCENT", "0"),
-          widthOf70Pct = convertToInt(input.head, "WIDTH_OF_70_PERCENT", "0"),
-          widthOf80Pct = convertToInt(input.head, "WIDTH_OF_80_PERCENT", "0"),
-          widthOf90Pct = convertToInt(input.head, "WIDTH_OF_90_PERCENT", "0"),
-          widthOf99Pct = convertToInt(input.head, "WIDTH_OF_99_PERCENT", "0")
+          widthOf10Pct = convertToInt(input.head, "WIDTH_OF_10_PERCENT", "-1"),
+          widthOf20Pct = convertToInt(input.head, "WIDTH_OF_20_PERCENT", "-1"),
+          widthOf30Pct = convertToInt(input.head, "WIDTH_OF_30_PERCENT", "-1"),
+          widthOf40Pct = convertToInt(input.head, "WIDTH_OF_40_PERCENT", "-1"),
+          widthOf50Pct = convertToInt(input.head, "WIDTH_OF_50_PERCENT", "-1"),
+          widthOf60Pct = convertToInt(input.head, "WIDTH_OF_60_PERCENT", "-1"),
+          widthOf70Pct = convertToInt(input.head, "WIDTH_OF_70_PERCENT", "-1"),
+          widthOf80Pct = convertToInt(input.head, "WIDTH_OF_80_PERCENT", "-1"),
+          widthOf90Pct = convertToInt(input.head, "WIDTH_OF_90_PERCENT", "-1"),
+          widthOf99Pct = convertToInt(input.head, "WIDTH_OF_99_PERCENT", "-1")
           )
         )
       case "PicardMeanQualByCycle" =>
         Right(new PicardMeanQualByCycle(
-        r1MeanQual = convertToDouble(input.head, "R1_MEAN_QUAL", "0.0"),
-        r2MeanQual = convertToDouble(input.head, "R2_MEAN_QUAL", "0.0")
+        r1MeanQual = convertToDouble(input.head, "R1_MEAN_QUAL", "-1.0"),
+        r2MeanQual = convertToDouble(input.head, "R2_MEAN_QUAL", "-1.0")
           )
         )
         case "RnaSeqQcStats" =>
@@ -105,75 +105,75 @@ class MapToAnalysisObject(mdType: String, input: List[Map[String, String]]) {
         sample = input.head.getOrElse("Sample", "N/A"),
         note = input.head.getOrElse("Note", "N/A"),
         alignmentMetrics = RnaSeqQcStats.AlignmentMetrics(
-          mapped = convertToInt(input.head, "Mapped", "0"),
-          mappedPairs = convertToInt(input.head, "Mapped Pairs", "0"),
-          mappingRate = convertToDouble(input.head, "Mapping Rate", "0.0"),
-          end1MappingRate = convertToDouble(input.head, "End 1 Mapping Rate", "0.0"),
-          end2MappingRate = convertToDouble(input.head, "End 2 Mapping Rate", "0.0"),
-          mappedUnique = convertToInt(input.head, "Mapped Unique", "0"),
-          uniqueRateofMapped = convertToDouble(input.head, "Unique Rate of Mapped", "0.0"),
-          mappedUniqueRateofTotal = convertToDouble(input.head, "Mapped Unique Rate of Total", "0.0"),
-          alternativeAlignments = convertToInt(input.head, "Alternative Alignments", "0"),
-          duplicationRateOfMapped = convertToDouble(input.head, "Duplication Rate of Mapped", "0")
+          mapped = convertToInt(input.head, "Mapped", "-1"),
+          mappedPairs = convertToInt(input.head, "Mapped Pairs", "-1"),
+          mappingRate = convertToDouble(input.head, "Mapping Rate", "-1.0"),
+          end1MappingRate = convertToDouble(input.head, "End 1 Mapping Rate", "-1.0"),
+          end2MappingRate = convertToDouble(input.head, "End 2 Mapping Rate", "-1.0"),
+          mappedUnique = convertToInt(input.head, "Mapped Unique", "-1"),
+          uniqueRateofMapped = convertToDouble(input.head, "Unique Rate of Mapped", "-1.0"),
+          mappedUniqueRateofTotal = convertToDouble(input.head, "Mapped Unique Rate of Total", "-1.0"),
+          alternativeAlignments = convertToInt(input.head, "Alternative Alignments", "-1"),
+          duplicationRateOfMapped = convertToDouble(input.head, "Duplication Rate of Mapped", "-1")
         ),
         annotationMetrics = RnaSeqQcStats.AnnotationMetrics(
-          rRNA = convertToInt(input.head, "rRNA", "0"),
-          rRNArate = convertToDouble(input.head, "rRNA Rate", "0.0"),
-          intragenicRate = convertToDouble(input.head, "Intragenic Rate", "0.0"),
-          exonicRate = convertToDouble(input.head, "Exonic Rate", "0.0"),
-          intergenicRate = convertToDouble(input.head, "Intergenic Rate", "0.0"),
-          intronicRate = convertToDouble(input.head, "Intronic Rate", "0.0"),
-          genesDetected = convertToInt(input.head, "Genes Detected", "0.0")
+          rRNA = convertToInt(input.head, "rRNA", "-1"),
+          rRNArate = convertToDouble(input.head, "rRNA Rate", "-1.0"),
+          intragenicRate = convertToDouble(input.head, "Intragenic Rate", "-1.0"),
+          exonicRate = convertToDouble(input.head, "Exonic Rate", "-1.0"),
+          intergenicRate = convertToDouble(input.head, "Intergenic Rate", "-1.0"),
+          intronicRate = convertToDouble(input.head, "Intronic Rate", "-1.0"),
+          genesDetected = convertToInt(input.head, "Genes Detected", "-1.0")
         ),
         covMetrics = RnaSeqQcStats.CovMetrics(
-          meanCV = convertToDouble(input.head, "Mean CV", "0.0"),
-          meanPerBaseCov = convertToDouble(input.head, "Mean Per Base Cov.", "0.0")
+          meanCV = convertToDouble(input.head, "Mean CV", "-1.0"),
+          meanPerBaseCov = convertToDouble(input.head, "Mean Per Base Cov.", "-1.0")
         ),
         endMetrics = RnaSeqQcStats.EndMetrics(
-          end1Sense = convertToInt(input.head, "End 1 Sense", "0"),
-          end1MismatchRate = convertToDouble(input.head, "End 1 Sense", "0"),
-          end1Antisense = convertToInt(input.head, "End 1 Antisense", "0"),
-          end1PctSense = convertToDouble(input.head, "End 1 % Sense", "0"),
-          end2Sense = convertToInt(input.head, "End 2 Sense", "0"),
-          end2MismatchRate = convertToDouble(input.head, "End 2 Sense", "0"),
-          end2Antisense = convertToInt(input.head, "End 2 Antisense", "0"),
-          end2PctSense = convertToDouble(input.head, "End 2 % Sense", "0"),
-          noCovered5Prime = convertToInt(input.head, "No Covered 5'", "0"),
-          fivePrimeNorm = convertToDouble(input.head, "5' Norm", "0.0")
+          end1Sense = convertToInt(input.head, "End 1 Sense", "-1"),
+          end1MismatchRate = convertToDouble(input.head, "End 1 Sense", "-1"),
+          end1Antisense = convertToInt(input.head, "End 1 Antisense", "-1"),
+          end1PctSense = convertToDouble(input.head, "End 1 % Sense", "-1"),
+          end2Sense = convertToInt(input.head, "End 2 Sense", "-1"),
+          end2MismatchRate = convertToDouble(input.head, "End 2 Sense", "-1"),
+          end2Antisense = convertToInt(input.head, "End 2 Antisense", "-1"),
+          end2PctSense = convertToDouble(input.head, "End 2 % Sense", "-1"),
+          noCovered5Prime = convertToInt(input.head, "No Covered 5'", "-1"),
+          fivePrimeNorm = convertToDouble(input.head, "5' Norm", "-1.0")
         ),
         gapMetrics = RnaSeqQcStats.GapMetrics(
-          numGaps = convertToInt(input.head, "Num. Gaps", "0"),
-          gapPct = convertToDouble(input.head, "Gap %", "0.0"),
-          cumulGapLength = convertToInt(input.head, "Cumul. Gap Length", "0")
+          numGaps = convertToInt(input.head, "Num. Gaps", "-1"),
+          gapPct = convertToDouble(input.head, "Gap %", "-1.0"),
+          cumulGapLength = convertToInt(input.head, "Cumul. Gap Length", "-1")
         ),
         readMetrics = RnaSeqQcStats.ReadMetrics(
-          chimericPairs = convertToInt(input.head, "Chimeric Pairs", "0"),
-          readLength = convertToInt(input.head, "Read Length", "0"),
-          unpairedReads = convertToInt(input.head, "Unpaired Reads", "0"),
-          fragmentLengthStdDev = convertToDouble(input.head, "Fragment Length Standard Deviation", "0.0"),
-          totalPurityFilteredReadsSequenced = convertToInt(input.head, "Total Purity Filtered Reads Sequenced", "0"),
-          fragmentLengthMean = convertToInt(input.head, "Fragment Length Mean", "0"),
-          baseMismatchRate = convertToDouble(input.head, "Base Mismatch Rate", "0.0"),
-          failedVendorQCCheck = convertToInt(input.head, "Failed Vendor QC Check", "0"),
-          estimatedLibrarySize = convertToInt(input.head, "Estimated Library Size", "0"),
-          expressionProfilingEfficiency = convertToDouble(input.head, "Expression Profiling Efficiency", "0.0"),
-          transcriptsDetected = convertToInt(input.head, "Transcripts Detected", "0")
+          chimericPairs = convertToInt(input.head, "Chimeric Pairs", "-1"),
+          readLength = convertToInt(input.head, "Read Length", "-1"),
+          unpairedReads = convertToInt(input.head, "Unpaired Reads", "-1"),
+          fragmentLengthStdDev = convertToDouble(input.head, "Fragment Length Standard Deviation", "-1.0"),
+          totalPurityFilteredReadsSequenced = convertToInt(input.head, "Total Purity Filtered Reads Sequenced", "-1"),
+          fragmentLengthMean = convertToInt(input.head, "Fragment Length Mean", "-1"),
+          baseMismatchRate = convertToDouble(input.head, "Base Mismatch Rate", "-1.0"),
+          failedVendorQCCheck = convertToInt(input.head, "Failed Vendor QC Check", "-1"),
+          estimatedLibrarySize = convertToInt(input.head, "Estimated Library Size", "-1"),
+          expressionProfilingEfficiency = convertToDouble(input.head, "Expression Profiling Efficiency", "-1.0"),
+          transcriptsDetected = convertToInt(input.head, "Transcripts Detected", "-1")
             )
           )
         )
       case "ErccStats" =>
         Right(new ErccStats(
-        totalReads = convertToInt(input.head, "TOTAL_READS", "0"),
-        totalErccReads = convertToInt(input.head,"ERCC_READS", "0"),
-        totalUnalignedReads = convertToInt(input.head,"UNALIGNED_ERCC_READS", "0"),
-        fractionGenomeReferenceReads = convertToDouble(input.head, "FRC_GENOME_REF", "0.0"),
-        fractionErccReads = convertToDouble(input.head, "FRC_ERCC_READS", "0.0"),
-        fractionUnalignedReads = convertToDouble(input.head, "FRC_UNALIGNED_ERCC", "0.0")
+        totalReads = convertToInt(input.head, "TOTAL_READS", "-1"),
+        totalErccReads = convertToInt(input.head,"ERCC_READS", "-1"),
+        totalUnalignedReads = convertToInt(input.head,"UNALIGNED_ERCC_READS", "-1"),
+        fractionGenomeReferenceReads = convertToDouble(input.head, "FRC_GENOME_REF", "-1.0"),
+        fractionErccReads = convertToDouble(input.head, "FRC_ERCC_READS", "-1.0"),
+        fractionUnalignedReads = convertToDouble(input.head, "FRC_UNALIGNED_ERCC", "-1.0")
         )
       )
       case "PicardMeanGc" =>
         Right(new PicardReadGcMetrics(
-        meanGcContent = convertToDouble(input.head, "MEAN_GC_CONTENT", "0.0")
+        meanGcContent = convertToDouble(input.head, "MEAN_GC_CONTENT", "-1.0")
         )
       )
       case _ => Left("unrecognized mdType input for MapToObject")
--- src/test/scala/MapToAnalysisObjectSpec.scala
+++ src/test/scala/MapToAnalysisObjectSpec.scala
@@ -1,12 +1,12 @@
 /**
   * Created by Amr on 9/16/2016.
   */
-import org.broadinstitute.MD.types.{AnalysisMetrics, PicardAlignmentSummaryAnalysis}
+import org.broadinstitute.MD.types._
 import org.scalatest._
 import org.broadinstitute.parsomatic.MapToAnalysisObject
 
 class MapToAnalysisObjectSpec extends FlatSpec with Matchers {
-  val test_entry = List(
+  val alnTestEntry = List(
     Map("CATEGORY" -> "FIRST_OF_PAIR",
       "TOTAL_READS" -> "1184046",
       "PF_READS" -> "1184046",
@@ -31,12 +31,102 @@ class MapToAnalysisObjectSpec extends FlatSpec with Matchers {
       "PCT_ADAPTER" -> "0.000028"
     )
   )
-  "a picard alignment mapper" should "return a PicardAlignmentSummaryAnalysis when given valid input" in {
-    val mapper = new MapToAnalysisObject("PicardAlignmentMetrics", test_entry)
-    mapper.go().right.get.isInstanceOf[PicardAlignmentSummaryAnalysis] should be(true)
+  val insTestEntry = List(
+    Map(
+      "MEDIAN_INSERT_SIZE" -> "1",
+      "MEDIAN_ABSOLUTE_DEVIATION" -> "1",
+      "MIN_INSERT_SIZE" -> "1",
+      "MAX_INSERT_SIZE" -> "1",
+      "MEAN_INSERT_SIZE" -> "1",
+      "STANDARD_DEVIATION" -> "1",
+      "READ_PAIRS" -> "1",
+      "PAIR_ORIENTATION" -> "F/R",
+      "WIDTH_OF_10_PERCENT" -> "1",
+      "WIDTH_OF_20_PERCENT" -> "1",
+      "WIDTH_OF_30_PERCENT" -> "1",
+      "WIDTH_OF_40_PERCENT" -> "1",
+      "WIDTH_OF_50_PERCENT" -> "1",
+      "WIDTH_OF_60_PERCENT" -> "1",
+      "WIDTH_OF_70_PERCENT" -> "1",
+      "WIDTH_OF_80_PERCENT" -> "1",
+      "WIDTH_OF_90_PERCENT" -> "1",
+      "WIDTH_OF_99_PERCENT" -> "1"
+    )
+  )
+  val mqcTestEntry = List(
+    Map(
+      "R1_MEAN_QUAL" -> "35",
+      "R2_MEAN_QUAL" -> "26"
+    )
+  )
+  "A PicardAlignmentMetrics mapper" should "return a PicardAlignmentSummaryAnalysis when given valid input" in {
+    val mapper = new MapToAnalysisObject("PicardAlignmentMetrics", alnTestEntry)
+    val metrics = mapper.go()
+    metrics.right.get.isInstanceOf[PicardAlignmentSummaryAnalysis]  should be (true)
+    metrics match {
+      case Right(r) => for (y <- r.toString.split(Array(',', '(', ')')).drop(4)) assert(y.toDouble > -1)
+      case Left(l) => println(l)
+    }
   }
-  "a picard alignment mapper" should "give unrecognized MD type message with bad MD type" in {
-    val mapper = new MapToAnalysisObject("foo", test_entry)
+  it should "give unrecognized MD type message with bad MD type" in {
+    val mapper = new MapToAnalysisObject("foo", alnTestEntry)
     mapper.go().left.get should be ("unrecognized mdType input for MapToObject")
+  }
+  it should "contain negative values to indicate a key for a parameter was not found" in {
+    val missingKeys = List(Map("foo" -> "bar"))
+    val mapper = new MapToAnalysisObject("PicardAlignmentMetrics", missingKeys)
+    val metrics = mapper.go()
+    metrics match {
+      case Right(r) => for (y <- r.toString.split(Array(',', '(', ')')).drop(4)) assert(y.toDouble < 0)
+      case Left(l) => println(l)
+    }
+  }
+  "A PicardInsertSizeMetrics mapper" should "return a PicardInsertSizeMetrics when given valid input" in {
+    val mapper = new MapToAnalysisObject("PicardInsertSizeMetrics", insTestEntry)
+    val metrics = mapper.go()
+    metrics.right.get.isInstanceOf[PicardInsertSizeMetrics]  should be (true)
+    metrics match {
+      case Right(r) => for (y <- List.concat(r.toString.split(Array(',', '(', ')')).slice(4,8), r.toString.split(Array(',', '(', ')')).slice(9,20))) {
+        assert(y.toDouble > -1)
+      }
+      case Left(l) => println(l)
+    }
+  }
+  it should "give unrecognized MD type message with bad MD type" in {
+    val mapper = new MapToAnalysisObject("foo", alnTestEntry)
+    mapper.go().left.get should be ("unrecognized mdType input for MapToObject")
+  }
+  it should "contain negative values to indicate a key for a parameter was not found" in {
+    val missingKeys = List(Map("foo" -> "bar"))
+    val mapper = new MapToAnalysisObject("PicardAlignmentMetrics", missingKeys)
+    val metrics = mapper.go()
+    metrics match {
+      case Right(r) => for (y <- List.concat(r.toString.split(Array(',', '(', ')')).slice(4,8), r.toString.split(Array(',', '(', ')')).slice(9,20))) {
+        assert(y.toDouble < 0)
+      }
+      case Left(l) => println(l)
+    }
+  }
+  "A PicardMeanQualByCycle mapper" should "return a PicardMeanQualByCycle when given valid input" in {
+    val mapper = new MapToAnalysisObject("PicardMeanQualByCycle", mqcTestEntry)
+    val metrics = mapper.go()
+    metrics.right.get.isInstanceOf[PicardMeanQualByCycle]  should be (true)
+    metrics match {
+      case Right(r) => for (y <- r.toString.split(Array(',', '(', ')')).drop(4)) assert(y.toDouble > -1)
+      case Left(l) => println(l)
+    }
+  }
+  it should "give unrecognized MD type message with bad MD type" in {
+    val mapper = new MapToAnalysisObject("foo", mqcTestEntry)
+    mapper.go().left.get should be ("unrecognized mdType input for MapToObject")
+  }
+  it should "contain negative values to indicate a key for a parameter was not found" in {
+    val missingKeys = List(Map("foo" -> "bar"))
+    val mapper = new MapToAnalysisObject("PicardAlignmentMetrics", missingKeys)
+    val metrics = mapper.go()
+    metrics match {
+      case Right(r) => for (y <- r.toString.split(Array(',', '(', ')')).drop(4)) assert(y.toDouble < 0)
+      case Left(l) => println(l)
+    }
   }
 }
