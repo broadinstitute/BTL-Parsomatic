5843379e86ffe7d8949b0c80de40c947e6fc4d72
amr <amr@49feb11a-dc6b-4521-947f-5d0cd0a85b6f>
2016-11-10 19:45:07 +0000

    --Added TODO comments post code-review.
    git-svn-id: https://svn.broadinstitute.org/BTL/Parsomatic@1304 49feb11a-dc6b-4521-947f-5d0cd0a85b6f

--- src/main/scala/org/broadinstitute/parsomatic/GetErccStats.scala
+++ src/main/scala/org/broadinstitute/parsomatic/GetErccStats.scala
@@ -7,6 +7,17 @@ package org.broadinstitute.parsomatic
   * Converts results of get_ercc_stats.py into format usable by Parsomatic.
   * @param input An iterator representing the ercc stats file.
   */
+// TODO: Can convert this to an object which is more efficient memory wise because you don't have to to make this
+// more than once. In general use objects when just using the thing one time. In this case, input can be moved to
+// the method call as opposed to being part of the class.
+// TODO: Make delimiter a variable.
+// TODO: Replace output of iterator with list.
+// TODO: I don't really need statsList.length==6. If I can make the list, what can I do with the list to know how many
+// Headers you have any can insert the delimiters.
+// TODO: Improve fail statement to be more specific about seeing X columns but expecting 6.
+// TODO: Use private val for any val that is only being used in the scope of the val exists in. This is good documentation
+// but also can possibly be more efficient for the compiler.
+
 class GetErccStats(input: Iterator[String]) {
   val stats = scala.collection.mutable.ListBuffer[String]("TOTAL_READS\tERCC_READS\tUNALIGNED_ERCC_READS\tFRC_GENOME_REF\tFRC_ERCC_READS\tFRC_UNALIGNED_ERCC")
 
--- src/main/scala/org/broadinstitute/parsomatic/GetPicardMeanGcMetrics.scala
+++ src/main/scala/org/broadinstitute/parsomatic/GetPicardMeanGcMetrics.scala
@@ -1,36 +1,54 @@
 package org.broadinstitute.parsomatic
 import com.typesafe.scalalogging.Logger
-
 import scala.math.BigDecimal.RoundingMode
 
 /**
   * Created by amr on 9/22/2016.
   */
+
+// TODO: Can convert this to an object which is more efficient memory wise because you don't have to to make this
+// more than once. In general use objects when just using the thing one time. In this case, input can be moved to
+// the method call as opposed to being part of the class.
+// TODO: Replace output of iterator with list.
+// TODO: Use private val for any val that is only being used in the scope of the val exists in. This is good documentation
+// but also can possibly be more efficient for the compiler.
+// TODO: print more informative error for case e: Exception
+// TODO: More comments in general.
+// TODO: Should never make a var public because should.
+// TODO: Can make this an object.
+// TODO: Use object instead of a class unless you are going to use the thing more than once.
 class GetPicardMeanGcMetrics(input: Iterator[String], delim: String) {
-  var observations = 0
+  var observations = 0 //TODO: Can use fold left instead of observations over a this as a tuple where I sum observations.
+  // as well as the other thing in this list. Inside the fold, use GetOrElse to defend against empty values.
   val logger = Logger("GetPicardMeanGcMetrics")
+
   def convertAndMultiply(str: String): Long = {
     val intList = str.split(delim)
+    // TODO: If intList length isn't 2 we should fail gracefully.
     observations += intList(1).toInt
     intList(0).toLong * intList(1).toLong
   }
 
   def getMean: Either[String, Iterator[String]] = {
     try {
+      //TODO: should use a header_length private val in class instead of the #2 so to make this more clear what the two is doing.
       val new_input = input.drop(2).toList
       logger.debug(s"Observations found: $observations")
       val sum_bin_totals = new_input.map(convertAndMultiply(_)).sum
+      //TODO: Fold here will eliminate convertandmutliply. Remember to use getOrElse.
       logger.debug(s"Sum of GC bins: $sum_bin_totals")
+      //TODO: Why did I use HALF_EVEN?
       val meanGc = BigDecimal(sum_bin_totals/observations).setScale(2, RoundingMode.HALF_EVEN)
       logger.debug(s"meanGc = $sum_bin_totals/$observations = $meanGc")
       if (meanGc > 0) {
         Right(Iterator("MEAN_GC_CONTENT", meanGc.toString))
       }
-      else Left("GetPicardMeanGcMetrics.getMean failed unexpectedly")
+      else Left("GetPicardMeanGcMetrics.getMean failed because meanGC was less than or greater than zero.")
     } catch {
-      case nfe: NumberFormatException => Left("GetPicardMeanGcMetrics.getMean failed:" + nfe.getMessage)
-      case _: Throwable => Left("GetPicardMeanGcMetrics.getMean failed with unknown error")
+      case nfe: NumberFormatException => Left("GetPicardMeanGcMetrics.getMean failed:" + nfe.getLocalizedMessage)
+      // TODO: Can make the error below more informative
+      case e: Exception => Left("GetPicardMeanGcMetrics.getMean failed with unknown error")
+      case _ => Left("GetPicardMeanGcMetrics.getMean failed with unknown error")
     }
-
   }
 }
