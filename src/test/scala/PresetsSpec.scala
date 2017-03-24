61ec0d0d7016a836a5b88850ace20667ce0d9fe7
amr <amr@49feb11a-dc6b-4521-947f-5d0cd0a85b6f>
2017-03-24 17:10:35 +0000

    git-svn-id: https://svn.broadinstitute.org/BTL/Parsomatic@1538 49feb11a-dc6b-4521-947f-5d0cd0a85b6f

--- src/test/scala/PresetsSpec.scala
+++ src/test/scala/PresetsSpec.scala
@@ -1,6 +1,46 @@
 /**
   * Created by amr on 10/3/2016.
   */
-class PresetsSpec {
+
+import org.broadinstitute.MD.types.SampleRef
+import org.scalatest.{FlatSpec, Matchers}
+import org.broadinstitute.parsomatic.Presets._
+import org.broadinstitute.parsomatic.{Config, ObjectToMd, ParsomaticParser}
+
+class PresetsSpec extends FlatSpec with Matchers {
+  "A SampleSheetPreset" should "produce correct sample sheet data in a db" in {
+    val config_1 = Config(
+      sampleId = "TestSample1",
+      setId = "TestSet1",
+      version = Some(1490358021350L),
+      inputFile = "C:\\Dev\\Scala\\Parsomatic\\src\\test\\resources\\input_data.tsv"
+    )
+
+    val config_2 = Config(
+      sampleId = "TestSample2",
+      setId = "TestSet2",
+      version = Some(1L),
+      inputFile = "C:\\Dev\\Scala\\Parsomatic\\src\\test\\resources\\input_data.tsv"
+    )
+
+    val ssp1 = new SampleSheetPreset(config_1)
+    val ssp2 = new SampleSheetPreset(config_2)
+    val preset = "SampleSheet"
+    val filterResult1 = ssp1.filter(ssp1.start, ssp1.end)
+    val filterResult2 = ssp2.filter(ssp2.start, ssp2.end)
+    // expected results
+    val frLength1 = 7
+    val frLength2 = 1
+    val ec1_bc1 = "TAAGGCGA"
+    val ec1_bc2 = "TATCCTCT"
+    val ec2_bc1 = "TAAGGCGA"
+    val ec2_bc2 = "GCGTAAGA"
+    val org = "human"
+    val dp = "/btl/data/walkup/SMART-Seq/SSF-2315/data/HHJTVBGX2/C1-66_2017-03-16_2017-03-16"
+    filterResult1.right.get.length shouldBe frLength1
+    filterResult2.right.get.length shouldBe frLength2
+    ssp1.run()
+    ssp2.run()
+    }
 
 }
